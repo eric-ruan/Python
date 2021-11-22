b513259ef30270efce0263ab1ba58ab562e55a50
Éric <ericrss2021@gmail.com>
2021-11-22 20:29:00 -0300

    Finalizando o módulo 2

--- Mundo 2/Exercicios com while e break/estatistica_em_produto.py
+++ Mundo 2/Exercicios com while e break/estatistica_em_produto.py
@@ -3,28 +3,34 @@ O programa deverá perguntar se o usuário vai continuar ou não. No final, most
 A) qual é o total gasto na compra. -
 B) quantos produtos custam mais de R$1000. -
 C) qual é o nome do produto mais barato. -'''
-
-'''cont = valor = totValor = contMais = contMenor = 0
+total = totalMil = menor = cont = 0
 print('-'*20)
 print('Mercearia do Éric')
 print('-'*20)
+barato = ''
 while True:
     nome = str(input('Digite o nome do produto: ')).upper().strip()
-    valor = float(input('Digite o valor do produto: R$'))
+    preco = float(input('Digite o valor do produto: R$'))
+    cont += 1
     print('-=-'*20)
-    totValor += valor
-    if valor > 1000:
-        contMais += 1
-    elif cont == 1:
-        if valor < contMenor:
-            contMenor = valor
-            #nomeProd = nome
+    total += preco
+#-----------------------------------------------
+    if preco > 1000:
+        totalMil += 1
+#-----------------------------------------------
+    if cont == 1:
+        menor = preco
+        barato = nome
+    else:
+        if preco < menor:
+            menor = preco
+            barato = nome
     
     resp = str(input('Deseja continuar [S/N]? ')).upper().strip()[0]
     print('-=-'*20)
     if resp in 'nN':
         break
 
-print(f'O valor total da compra é de {totValor}')
-print(f'O número de produtos que custa menos de R$1000.00 é {contMais}')
-print(f'O nome do produto mais barato é {contMenor}')'''
+print(f'O valor total da compra é de {total:.2f}')
+print(f'O número de produtos que custa menos de R$1000.00 é {totalMil}')
+print(f'O nome do produto mais barato é {barato}, seu valor é de R${menor:.2f}')
--- Mundo 2/Exercicios com while e break/simulador_de_caixa_eletronico.py
+++ Mundo 2/Exercicios com while e break/simulador_de_caixa_eletronico.py
@@ -2,4 +2,28 @@
 No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
 e o programa vai informar quantas cédulas de cada valor serão entregues.
 OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
-
+print('='*30)
+print('{:^30}'.format('Banco ER'))
+print('='*30)
+valor = int(input('Quantos reais será sacado: R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:   
+            print(f'Total de {totced} cedulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('='*30)
+print('Volte sempre')
